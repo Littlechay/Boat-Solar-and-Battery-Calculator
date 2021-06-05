2ea776153d3df6b4c7a6411df90ddf873fadfe4c
Chris Harris <chrisharris@pop-os.localdomain>
2021-06-06 00:34:02 +1200

    formatting

--- solar.js
+++ solar.js
@@ -1,16 +1,35 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("battery");
 var paneloutput = document.getElementById("panelout");
+var altOutput = document.getElementById("alternator");
+var altSlider = document.getElementById("myAlternator");
+var solarOutput = document.getElementById("solarP");
+var solarSlider = document.getElementById("mySolarPanel");
+var windOutput = document.getElementById("windG");
+var windSlider = document.getElementById("myWindGen");
+
 
 output.innerHTML = slider.value; // Display the default slider value
+paneloutput.innerHTML = 0;
+altOutput.innerHTML = myAlternator.value;
+solarOutput.innerHTML = mySolarPanel.value;
+windOutput.innerHTML = myWindGen.value;
+
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function () {
   output.innerHTML = this.value;
-  paneloutput.innerHTML = this.value * 2;
+  capacityTotalBatt.innerHTML = this.value;
+  capacityUsable.innerHTML = this.value * 0.3;
+  paneloutput.innerHTML = this.value * 0.8;
 
 }
-function testResults(form) {
-  var TestVar = form.inputbox.value;
-  alert("You typed: " + TestVar);
+altSlider.oninput = function () {
+  altOutput.innerHTML = this.value;
+}
+solarSlider.oninput = function () {
+  solarOutput.innerHTML = this.value;
+}
+windSlider.oninput = function () {
+  windOutput.innerHTML = this.value;
 }
